0c482d554d92e56dd969ff7e737cc2bc48df4e62
Kovalov Oleksandr <oleksandr.k@volpis.com>
2022-10-24 14:01:37 +0300

    Initial Commit

---
+++ HolyQuranTest/HolyQuranTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  HolyQuranTestApp.swift
+//  HolyQuranTest
+//
+//  Created by Oleksandr Kovalov on 24.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct HolyQuranTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
